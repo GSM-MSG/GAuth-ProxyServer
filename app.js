31f687759288b2194a5f669fd1b9d64220d15e38
baegteun <baegteun@gmail.com>
2023-01-04 13:49:46 +0900

    :tada: ::

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { getLinkPreview } = require('link-preview-js');
+
+const app = express();
+
+const port = '3003';
+
+app.get('/:url(*)', (req, res) => {
+  const url = req.params.url
+  getLinkPreview(url)
+    .then(data => {
+      if (data.images[0]) 
+        res.send({
+          'image': data.images[0]
+        });
+      else
+        res.status(404).send('Not found twitter image')
+    })
+    .catch(_ => {
+      res.status(404).send('Not found twitter image');
+    });
+});
+
+app.listen(port);
